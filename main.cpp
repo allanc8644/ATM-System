467c5d2229a78d482a9ff4f483038f728ad7b503
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-01-10 15:14:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+  
+  //create a menu
+  
+  cout << "Welcome to the ATM System. Please choose an option." << endl;
+  cout << endl;
+  cout << "1. Deposit" << endl;
+  cout << "2. Withdraw" << endl;
+  cout << "3. Check Balance" << endl;
+  cout << "4. Quit" << endl;
+ 
+  
+  
 
 
 
