29019be35de0c6037e64485c045402d26bc83d71
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-08-05 19:58:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,109 @@
 #include <iostream>
+#include <cctype>
+#include <iomanip>
+#include <windows.h>
+
+#include "Account.h"
 
 using namespace std;
 
+void displayMenu();
+void makeDeposit(Account &);
+void withdraw(Account&);
+
 int main() {
-  
-  //create a menu
-  
-  cout << "Welcome to the ATM System. Please choose an option." << endl;
-  cout << endl;
-  cout << "1. Deposit" << endl;
-  cout << "2. Withdraw" << endl;
-  cout << "3. Check Balance" << endl;
-  cout << "4. Quit" << endl;
- 
-  
-  
+	
+	system("Color A");
+	
+	Account savings;
+	char choice;
 
+	cout << fixed << showpoint << setprecision(2);
 
+	do
+	{
+		displayMenu();
+		cin >> choice;
+		while (toupper(choice) < 'A' || toupper(choice) > 'G')
+		{
+			cout << "Please make a choice in the range of A through G: ";
+			cin >> choice;
+		}
 
+		switch (choice)
+		{
+		case 'a':
+		case 'A':
+			cout << "The current balance is $";
+			cout << savings.getBalance() << endl;
+			break;
 
+		case 'b':
+		case 'B':
+			cout << "There have been ";
+			cout << savings.getTransactions();
+			cout << " transactions." << endl;
+			break;
 
-};
+		case 'c':
+		case 'C':
+			cout << "Interest earned for this period: $";
+			cout << savings.getInterest();
+			break;
+
+		case 'd':
+		case 'D':
+			makeDeposit(savings);
+			break;
+
+		case 'e':
+		case 'E':
+			withdraw(savings);
+			break;
+
+		case 'f':
+		case 'F':
+			savings.calcInterest();
+			cout << "Interest added.";
+			break;
+		}
+
+	} while (toupper(choice) != 'G');
+
+	return 0;
+}
+
+void displayMenu() {
+
+	cout << "       MENU         " << endl;
+	cout << "--------------------" << endl;
+	cout << "(A) Display current balance" << endl;
+	cout << "(B) Display current number of transactions." << endl;
+	cout << "(C) Display interest earned" << endl;
+	cout << "(D) Make a deposit" << endl;
+	cout << "(E) Make a withdrawal" << endl;
+	cout << "(F) Add interest for this period" << endl;
+	cout << "(G) Exit the program" << endl;
+	cout << "Enter your choice" << endl;
+
+}
+
+void makeDeposit(Account& acnt) {
+	
+	double dollars;
+	cout << "Enter the amount for the deposit" << endl;
+	cin >> dollars;
+	cin.ignore();
+	acnt.makeDeposit(dollars);
+}
+
+void withdraw(Account& acnt) {
+	double dollars;
+	cout << "Enter the amount to withdraw." << endl;
+	cin >> dollars;
+	cin.ignore();
+
+	if (!acnt.withdraw(dollars))
+		cout << "ERROR. Withdrawal amount too large." << endl;
+
+}
